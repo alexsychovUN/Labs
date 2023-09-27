99cb1ff9701412ea5e339057dd95480882ad2d31
Alex <alexandersychev2005@gmail.com>
2023-09-27 22:43:46 +0300

    Update the second task wuth the Windows-1251 encoding(Cyrillic)

--- lab1.2/lab1.2.cpp
+++ lab1.2/lab1.2.cpp
@@ -1,15 +1,25 @@
-#include <iostream>
+﻿#include <iostream>
+#include <Windows.h>
 using namespace std;
+
 
 // Function declaration
 float Sum(float weight);
 
+
 int main() {
-    float weight;
-    cout << "Apples weight(kg): ";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP( 1251 );
+   
+
+    float weight, priceForOne;
+    cout << "Введіть початкові дані:  " << endl;
+    cout << "Ціна одного кілограма яблук(крб.):  ";
+    cin >> priceForOne;
+    cout << "Вага яблук(кг): ";
     cin >> weight;
     float answer = Sum(weight);
-    cout << "Apples price for " << weight << " kg = " << answer;
+    cout << "Вартість покупки " << answer << " грн.";
 }
 
 float Sum(float weight) {
