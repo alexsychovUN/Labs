b183f8ab8f49a0da3849cefbe053fbabb0a4fb30
Alex <alexandersychev2005@gmail.com>
2023-09-29 13:39:47 +0300

    added lab 2.1

--- lab2.1/lab2.1.cpp
+++ lab2.1/lab2.1.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
+
 
 // Var 22
-
+using namespace std;
 int main() {
 	double a = 0.9;
 	double x;
-	cout << "Enter the x for the formula: ";
+	double result;
+	cout << "Enter X: ";
 	cin >> x;
 
-	if (x > )
+	if (x > 3.5) {
+		result = sin(x) * log10(x);
+	}
+	else {
+		result = pow(cos(a * x), 2);
+	}
+	cout << "Y: " << result;
 
 
 }
