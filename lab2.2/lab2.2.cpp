f7f5538d3ce2d2121593278f1698fcdf5b1cabd7
Alex <alexandersychev2005@gmail.com>
2023-10-03 14:12:15 +0300

    lab 2.2 reworked

--- lab2.2/lab2.2.cpp
+++ lab2.2/lab2.2.cpp
@@ -12,6 +12,11 @@ void main() {
 	
 	double x;
 	char choice;
+	float f;
+	float y;
+	float tax;
+	float paidWithTax;
+
 
 	
 	cout << "Type the X: ";
@@ -21,34 +26,29 @@ void main() {
 	cin >> choice;
 
 	if (choice == '1') {
-		float f = 11.2 * cos(2 * x - 1) + abs(sin(1.5)) / 1.7;
-		float y = 100 * abs(f + 50);
-		float tax = y / 100 * 10;
-		float paidWithTax = y - tax;
-
-		cout << "Amount paid for work: " << y << endl;
-		cout << "The tax is: " << tax << endl;
-		cout << "Paymant with tax: " << paidWithTax;
+		f = 11.2 * cos(2 * x - 1) + abs(sin(1.5)) / 1.7;
+		y = 100 * abs(f + 50);
+		tax = y / 100 * 10;
+		paidWithTax = y - tax;
 	}
 	else if (choice == '2') {
-		float f = 9.2 * cos(2 * x) - abs(sin(x) / 1.1);
-		float y = 150 * abs(f + 100);
-		float tax = y / 100 * 15;
-		float paidWithTax = y - tax;
-
-		cout << "Amount paid for work: " << y << endl;
-		cout << "The tax is: " << tax << endl;
-		cout << "Paymant with tax: " << paidWithTax;
+		f = 9.2 * cos(2 * x) - abs(sin(x) / 1.1);
+		y = 150 * abs(f + 100);
+		tax = y / 100 * 15;
+		paidWithTax = y - tax;
 	}
 	else if (choice == '3') {
-		float f = 12.4 * sin(abs(x / 2.1)) - 8.3 * cos(1.2 * x);
-		float y = 100 * abs(f + 135);
-		float tax = y / 100 * 20;
-		float paidWithTax = y - tax;
+		f = 12.4 * sin(abs(x / 2.1)) - 8.3 * cos(1.2 * x);
+		y = 100 * abs(f + 135);
+		tax = y / 100 * 20;
+		paidWithTax = y - tax;
 	}
 	else {
 		cout << "Incorrect enter, try again!";
 		goto start;
 	}
+	cout << "Amount paid for work: " << y << endl;
+	cout << "The tax is: " << tax << endl;
+	cout << "Paymant with tax: " << paidWithTax;
 	
 }
